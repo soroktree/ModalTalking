cdfe7207e22e20d3a5cceeb9c7deee25f7624a6e
soroktree <soroktree@gmail.com>
2022-09-07 22:12:07 +0300

    hidemodal stimulus controller

---
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="turbo-modal"
+export default class extends Controller {
+  connect() {
+    console.log('xxx')
+  }
+  hideModal() {
+    console.log('close')
+    this.element.parentElement.removeAttribute("src") // it might be nice to also remove the modal SRC
+    this.element.remove()
+  }
+}
